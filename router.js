89dfd5392c38641f58a0b8bfcbe50173786bdf8d
polat-poyraz <poyrazpolat647@gmail.com>
2021-01-31 01:08:26 +0300

    changes in the main warehouse have been taken

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ import { Component } from "../system-functions/component.js"
 import Settings from "../../settings.js"
 
 export class Router {
-    constructor({ routerMode = "path", rootName = "body", routers = [], defaultComponent }) {
+    constructor({ linkChanged, routerMode = "path", rootName = "body", routers = [], defaultComponent }) {
         this.root = document.querySelector(rootName)
         this.rootName = rootName
         this.routers = routers
@@ -10,53 +10,74 @@ export class Router {
         this[routerMode] = location[routerMode]
         this.defaultComponent = defaultComponent
         this.routerMode = routerMode
+        this.linkChanged = linkChanged
+        this.title = ""
+        this.name = ""
 
+        this.createRouterObject()
+        this.routeManager()
         this.eventHandler()
-        this.routeManager(true)
+    }
+    
+    createRouterObject() {
+        window.vanille.$router = {
+            ...this
+        }
+    }
+
+    updateRouterObject = () => window.vanille.$router = Object.assign(window.vanille.$router, this)
+
+    setLink(to) {
+        if (this.linkChanged) {
+            if (this.routerMode === "pathname") {
+                history.pushState({}, "", to)
+            } else if (this.routerMode === "hash") {
+                location.hash = to
+            }
+        }
     }
 
     eventHandler() {
         document.querySelectorAll(this.routerLinkTagName).forEach(element => {
             element.addEventListener("click", () => {
-                const to = element.getAttribute("to"),
-                    title = element.getAttribute("title")
+                const to = element.getAttribute("to")
 
                 this[this.routerMode] = to
 
-                if (this.routerMode === "pathname") {
-                    history.pushState({}, "", to)
-                } else if (this.routerMode === "hash") {
-                    location.hash = to
-                }
-
-                title !== null ? document.title = title : null
+                this.setLink(to)
 
                 this.routeManager()
             })
         })
     }
 
-    getHash = () => (location.hash)
-
-    routeManager(onloadMode) {
+    routeManager() {
         let defaultMode = false
 
         for (const i in this.routers) {
             const req = this.routers[i][this.routerMode],
-                component = this.routers[i].component
+                { component, title, name } = this.routers[i]
 
             if (req === this[this.routerMode]) {
                 this.root.innerHTML = ""
+                
                 new Component(component).$render(this.rootName, {}, {}, true)
+
+                this.title = title
+                title !== undefined ? document.title = this.title : null
+                this.name = name !== undefined ? name : ""
                 defaultMode = false
+                this.updateRouterObject()
 
                 break
-            } else
+            } else {
                 defaultMode = true
+            }
         }
 
         if (defaultMode) {
             this.root.innerHTML = ""
+
             new Component(this.defaultComponent).$render(this.rootName, {}, {}, true)
         }
     }
